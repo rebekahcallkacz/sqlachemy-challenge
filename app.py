2ff472697e1d0e1da5e41028ff8573995b787cc2
Rebekah Callari-Kaczmarczyk <rebekahcallari@gmail.com>
2020-11-07 15:08:09 -0500

    initial flask api

--- app.py
+++ app.py
@@ -1,1 +1,68 @@
-# This script creates a Flask API
+# This script creates a Flask API to store and retrieve climate data from Hawaii.
+
+import numpy as np
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
+
+# Database Setup
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+
+# Reflect an existing database into a new model
+Base = automap_base()
+# Reflect the tables
+Base.prepare(engine, reflect=True)
+
+# Save reference to the tables
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+# Flask Setup
+app = Flask(__name__)
+
+# Flask Routes
+
+@app.route('/')
+def home():
+    print("Requesting home page...")
+    return(
+        f'Available Routes:<br/>'
+        f'Precipitation measured by day: /api/v1.0/precipitation'
+        f'List of stations: /api/v1.0/stations'
+        f'Dates and temperature observations of most active station: /api/v1.0/tobs'
+        f'Min, avg and max temperatures on or after given date: /api/v1.0/<start>'
+        f'Min, avg and max temperatures for date range: /api/v1.0/<start>/<end>'
+    )
+
+if __name__ == '__main__':
+    app.run(debug=True)
+'''
+
+
+* `/api/v1.0/precipitation`
+
+  * Convert the query results to a dictionary using `date` as the key and `prcp` as the value.
+
+  * Return the JSON representation of your dictionary.
+
+* `/api/v1.0/stations`
+
+  * Return a JSON list of stations from the dataset.
+
+* `/api/v1.0/tobs`
+  * Query the dates and temperature observations of the most active station for the last year of data.
+  
+  * Return a JSON list of temperature observations (TOBS) for the previous year.
+
+* `/api/v1.0/<start>` and `/api/v1.0/<start>/<end>`
+
+  * Return a JSON list of the minimum temperature, the average temperature, and the max temperature for a given start or start-end range.
+
+  * When given the start only, calculate `TMIN`, `TAVG`, and `TMAX` for all dates greater than and equal to the start date.
+
+  * When given the start and the end date, calculate the `TMIN`, `TAVG`, and `TMAX` for dates between the start and end date inclusive.
+  '''
